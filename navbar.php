3c53c283caac8e1b8ae952a3d83b9cdb66eb223c
delasalle-sio-durand-f <delasalle.sio.durand.f@gmail.com>
2016-11-28 10:51:01 +0100

    Modification de la section Jeux, corrections sur le fichier navbar.php et les mentions légales.

--- navbar.php
+++ navbar.php
@@ -5,13 +5,22 @@
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 						<span class="fa fa-bars color-white"></span>
 					</button>
-					<h1><a class="navbar-brand" href="index.html" data-0="line-height:90px;" data-300="line-height:50px;">			DLS ARENA 4
+					<h1><a class="navbar-brand" href="index.php" data-0="line-height:90px;" data-300="line-height:50px;">			DLS ARENA 4
 					</a></h1>
 				</div>
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav" data-0="margin-top:20px;" data-300="margin-top:5px;">
-						<li class="active"><a href="index.php">Accueil</a></li>
-						<li><a href="#featured">Présentation</a></li>
+						<li class="active"></li>
+						<li><?php
+                            if ($_SERVER['PHP_SELF']=="/dls/index.php")
+                            {
+                                echo'<a href="#featured">Accueil</a>';
+                            }
+                            else
+                            {
+                                echo'<a href="index.php">Accueil</a>';
+                            };
+                            ?></li>
 						<li><a href="#section-services">Les Jeux</a></li>
 						<li><a href="#section-about">L'Equipe</a></li>
                         <li><a href="#section-Login"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>
