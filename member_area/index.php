3467b6bb8e83e6ed5895d5568003f49e358a0ddf
delasalle-sio-causer-a <delasalle.sio.causer.a@gmail.com>
2016-12-13 14:32:46 +0100

    navbar

--- member_area/index.php
+++ member_area/index.php
@@ -5,9 +5,7 @@ session_start();
     <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
     <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
     <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-    <!--[if gt IE 8]><!-->
-    <html class="no-js">
-    <!--<![endif]-->
+    <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 
     <head>
         <!-- BASICS -->
@@ -102,6 +100,25 @@ session_start();
 
 
     ?>
+
+       <a href="#header" class="scrollup"><i class="fa fa-chevron-up"></i></a>
+
+	<script src="../js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	<script src="../js/jquery.js"></script>
+	<script src="../js/jquery.easing.1.3.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+
+	<script src="../js/jquery.isotope.min.js"></script>
+	<script src="../js/jquery.nicescroll.min.js"></script>
+	<script src="../js/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="../js/skrollr.min.js"></script>
+	<script src="../js/jquery.scrollTo-1.4.3.1-min.js"></script>
+	<script src="../js/jquery.localscroll-1.2.7-min.js"></script>
+	<script src="../js/stellar.js"></script>
+	<script src="../js/jquery.appear.js"></script>
+	<script src="../js/validate.js"></script>
+    <script src="../js/main.js"></script>
+
         </body>
 
     </html>
